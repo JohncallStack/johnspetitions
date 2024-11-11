ab4df8469a718afd136977bf7afee17d14cbb6e1
JohncallStack <157517331+JohncallStack@users.noreply.github.com>
2024-11-11 17:13:05 +0000

    1st Commit

---
+++ src/main/java/com/example/johnspetitions/JohnspetitionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.johnspetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JohnspetitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JohnspetitionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/johnspetitions/JohnspetitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.johnspetitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JohnspetitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
